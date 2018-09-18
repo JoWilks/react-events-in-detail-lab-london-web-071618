09bd5abf95962718e52c4f2129b8f7097ff541b7
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-18 18:05:30 +0100

    Jo

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class CoordinatesButton extends React.Component {
+
+    getCoordinates = (event) => {
+        let mouseCoordinates = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(mouseCoordinates)
+    }
+
+    render () {
+        return (<button onClick={this.getCoordinates}>
+            coordinates
+        </button>)
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component {
+
+    usePropsInfo = (event) => {
+        event.persist();
+        setTimeout(()=>this.props.onDelayedClick(event), this.props.delay)
+    }    
+
+    render () {
+        console.log(this.props)
+        return (
+        <button 
+        onClick={this.usePropsInfo} >
+        delay
+        </button>)
+    }
+}
